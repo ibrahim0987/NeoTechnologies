7883ff0ad88021b1864ed5bb64fb239a6f343ab8
Ibrahim Omais <63076706+ibrahim0987@users.noreply.github.com>
2023-10-01 01:08:11 +0300

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,19 @@
 variable "aws_region" {
   description = "AWS region for resources"
-  default     = "us-east-1"
+  default     = "eu-north-1"
 }
 
 variable "instance_ami" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-03a6eaae9938c858c"
+  default     = "ami-0703b5d7f7da98d1e"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
   description = "Name of your EC2 key pair"
-  default     = "ghost-key-pair"
+  default     = "My-Ghost-Key-Pair"
 }
